775a5343aa098b930cb06a0f9a2cb57b8705c1d2
zajhu <120088952+zajhu@users.noreply.github.com>
2022-12-08 09:25:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foto cat</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <img src="https://cdn.create.vista.com/downloads/787b32d8-8b48-40ee-ac5e-6ccebfa4751e_1024.jpeg" alt="logo kotki łaskotki"><h1>Kociaki</h1>
+    </header>
+    <main>
+        <section class="card_Contener">
+        <div class="card">
+            <img src="https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=230,height=230,fit=cover/animal/breed/cat/list/5cd03967e2843394027330.jpg" alt="fotografia kota rasy, Maine coon">
+            <article>
+                <header>
+                <h2 class="card_Tittle">Maine coon</h2>
+                </header>
+                Kot maine coon jest prawdziwym gigantem - to  największy kot domowy! Ten koci olbrzym jest nie tylko duży, ale także silny i mocno zbudowany. Ze względu na swe gęste futro, pokaźne rozmiary i pędzelki na uszach może przypominać rysia – zwłaszcza odmiany brązowe tabby. I mimo że nie do końca wiadomo, skąd maine coon pochodzi, w szerokich kręgach uważa się go za pierwszą rasę amerykańską. Jest energiczny i miły, co czyni go niezrównanym towarzyszem życia!
+            </article>
+        </div>
+
+        <div class="card">
+            <img src="https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=230,height=230,fit=cover/animal/breed/cat/list/5cd047a190128603744649.jpg" alt="fotografia kota rasy, Kot syjamski">
+            <article>
+                <header>
+                <h2 class="card_Tittle">Kot syjamski</h2>
+                </header>
+                Pod względem elegancji kot syjamski nie ma sobie równych. Często określa się go mianem „księcia pośród kotów”. Elegancki chód, trójkątny pyszczek i duże spiczaste uszy nadają mu wdzięku. W jego intensywnie niebieskim spojrzeniu można się wręcz zatracić… Podczas gdy wdzięk jest jego zaletą, syjam ma też pewien słaby punkt, który ujawnia się szczególnie wówczas, gdy kot otwiera pyszczek, żeby się odezwać. W każdym razie niektórzy tak uważają.</article>
+        </div>
+
+        <div class="card">
+            <img src="https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=230,height=230,fit=cover/animal/breed/cat/list/5cd03967e2843394027330.jpg" alt="fotografia kota rasy, Maine coon">
+            <article>
+                <header>
+                <h2 class="card_Tittle">Maine coon</h2>
+                </header>
+                Kot maine coon jest prawdziwym gigantem - to  największy kot domowy! Ten koci olbrzym jest nie tylko duży, ale także silny i mocno zbudowany. Ze względu na swe gęste futro, pokaźne rozmiary i pędzelki na uszach może przypominać rysia – zwłaszcza odmiany brązowe tabby. I mimo że nie do końca wiadomo, skąd maine coon pochodzi, w szerokich kręgach uważa się go za pierwszą rasę amerykańską. Jest energiczny i miły, co czyni go niezrównanym towarzyszem życia!
+            </article>
+        </div>
+
+        <div class="card">
+            <img src="https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=230,height=230,fit=cover/animal/breed/cat/list/5cd03967e2843394027330.jpg" alt="fotografia kota rasy, Maine coon">
+            <article>
+                <header>
+                <h2 class="card_Tittle">Maine coon</h2>
+                </header>
+                Kot maine coon jest prawdziwym gigantem - to  największy kot domowy! Ten koci olbrzym jest nie tylko duży, ale także silny i mocno zbudowany. Ze względu na swe gęste futro, pokaźne rozmiary i pędzelki na uszach może przypominać rysia – zwłaszcza odmiany brązowe tabby. I mimo że nie do końca wiadomo, skąd maine coon pochodzi, w szerokich kręgach uważa się go za pierwszą rasę amerykańską. Jest energiczny i miły, co czyni go niezrównanym towarzyszem życia!
+            </article>
+        </div>
+
+        <div class="card">
+            <img src="https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=230,height=230,fit=cover/animal/breed/cat/list/5cd03967e2843394027330.jpg" alt="fotografia kota rasy, Maine coon">
+            <article>
+                <header>
+                <h2 class="card_Tittle">Maine coon</h2>
+                </header>
+                Kot maine coon jest prawdziwym gigantem - to  największy kot domowy! Ten koci olbrzym jest nie tylko duży, ale także silny i mocno zbudowany. Ze względu na swe gęste futro, pokaźne rozmiary i pędzelki na uszach może przypominać rysia – zwłaszcza odmiany brązowe tabby. I mimo że nie do końca wiadomo, skąd maine coon pochodzi, w szerokich kręgach uważa się go za pierwszą rasę amerykańską. Jest energiczny i miły, co czyni go niezrównanym towarzyszem życia!
+            </article>
+        </div>
+
+        <div class="card">
+            <img src="https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=230,height=230,fit=cover/animal/breed/cat/list/5cd03967e2843394027330.jpg" alt="fotografia kota rasy, Maine coon">
+            <article>
+                <header>
+                <h2 class="card_Tittle">Maine coon</h2>
+                </header>
+                Kot maine coon jest prawdziwym gigantem - to  największy kot domowy! Ten koci olbrzym jest nie tylko duży, ale także silny i mocno zbudowany. Ze względu na swe gęste futro, pokaźne rozmiary i pędzelki na uszach może przypominać rysia – zwłaszcza odmiany brązowe tabby. I mimo że nie do końca wiadomo, skąd maine coon pochodzi, w szerokich kręgach uważa się go za pierwszą rasę amerykańską. Jest energiczny i miły, co czyni go niezrównanym towarzyszem życia!
+            </article>
+        </div>
+        </section>
+        
+    </main>
+    <footer>
+    
+    </footer>
+</body>
+</html>
